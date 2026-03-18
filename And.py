435b669638c93f33ed9c0c2e1331f223b1e69c8c
liuharry583 <shenmiren@sjtu.edu.cn>
2026-03-18 21:42:17 +0800

    习了 And 用法

---
+++ And.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+True and True  # True
+True and False  # False
+False and True  # False
+False and False  # False
+
+[True] and [True]  # [True]
+[True, True] and [True, False]  # [True, False]
+
+print(np.array([True, True]))  # [ True  True]
+
+np.array([True, False]) and np.array(
+    [True, True]
+)  # ValueError: The truth value of an array with more than one element is ambiguous. Use a.any() or a.all()
+print(np.array([True, False]) & np.array([True, True]))  # [ True False]
