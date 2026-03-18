9d91f2f489dad6d826a18c109f9ce08442bd78de
liuharry583 <shenmiren@sjtu.edu.cn>
2026-03-18 21:55:15 +0800

    函数/方法学习

--- And.py
+++ And.py
@@ -14,3 +14,4 @@ np.array([True, False]) and np.array(
     [True, True]
 )  # ValueError: The truth value of an array with more than one element is ambiguous. Use a.any() or a.all()
 print(np.array([True, False]) & np.array([True, True]))  # [ True False]
+...
